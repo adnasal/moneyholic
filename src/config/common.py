29394830c0958a4fb35f55b1806e625160059b16
adnasal <adnasalkovic@hotmail.com>
2022-10-03 17:00:19 +0200

    Fixing workflows.

--- src/config/common.py
+++ src/config/common.py
@@ -121,6 +121,18 @@ DATABASES = {
     }
 }
 
+if os.environ.get('GITHUB_WORKFLOW'):
+    DATABASES = {
+        'default': {
+           'ENGINE': 'django.db.backends.postgresql',
+           'NAME': 'github_actions',
+           'USER': 'postgres',
+           'PASSWORD': 'postgres',
+           'HOST': '127.0.0.1',
+           'PORT': '5432',
+        }
+    }
+
 # General
 APPEND_SLASH = True
 TIME_ZONE = 'UTC'
