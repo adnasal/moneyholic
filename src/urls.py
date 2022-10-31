a050891e99ef183e4725e276c8d20e326d8c7fb2
Adna Salković <adnasalkovic@hotmail.com>
2022-10-31 14:36:43 +0100

    Add files via upload

--- src/urls.py
+++ src/urls.py
@@ -9,8 +9,10 @@ from drf_yasg.views import get_schema_view
 from rest_framework.routers import DefaultRouter
 
 from newscraper.views import SymbolRemoveView, SymbolAddView, SymbolListView, ArticleListView, ArticleRemoveView, \
-    SymbolUpdateView, ArticleView, ArticleRecentNewsView, DeletedArticlesView, ArchivedArticlesView, ArticleArchiveView, \
-    ArticleDeleteView, CommentAddView, ArticleViewComments
+    SymbolUpdateView, ArticleView, ArticleRecentNewsView, DeletedArticlesView, ArchivedArticlesView,\
+    ArticleArchiveView, ArticleDeleteView, CommentAddView, ArticleViewComments, CommentRemoveView, \
+    ArticleReactionView, ArticleReactionAddView, CommentReactionView, CommentReactionAddView, WordcountListView, \
+    Top15Keywords, ArticleMostLikes, CommentMostLikes, ArticleMostComments
 from src.files.urls import files_router
 from src.users.urls import users_router
 
@@ -20,6 +22,8 @@ schema_view = get_schema_view(
 )
 
 router = DefaultRouter()
+
+app_name = "newscraper"
 
 router.registry.extend(users_router.registry)
 router.registry.extend(files_router.registry)
@@ -33,19 +37,32 @@ urlpatterns = [
                   path('summernote/', include('django_summernote.urls')),
                   # api
                   url(r'^api/v1/news', ArticleListView.as_view(), name='news'),
+                  url(r'^api/v1/top_keywords', Top15Keywords.as_view(), name='top_keywords'),
                   url(r'^api/v1/recent', ArticleRecentNewsView.as_view(), name='recent_news'),
                   url(r'^api/v1/deleted', DeletedArticlesView.as_view(), name='deleted_news'),
                   url(r'^api/v1/archived', ArchivedArticlesView.as_view(), name='archived_news'),
-                  url('api/v1/is_archived_article/(?P<pk>\d+)/$', ArticleArchiveView.as_view(), name='archive_article'),
-                  url('api/v1/is_deleted_article/(?P<pk>\d+)/$', ArticleDeleteView.as_view(), name='delete_article'),
-                  url('api/v1/remove_article/(?P<pk>\d+)/$', ArticleRemoveView.as_view(), name='remove_article'),
-                  url('api/v1/article/(?P<pk>\d+)/$', ArticleView.as_view(), name='get_article'),
-                  url('api/v1/article_comments/(?P<pk>\d+)/$', ArticleViewComments.as_view(), name='get_article_comments'),
+                  url('api/v1/is_archived_article', ArticleArchiveView.as_view(), name='archive_article'),
+                  url('api/v1/is_deleted_article', ArticleDeleteView.as_view(), name='delete_article'),
+                  url('api/v1/remove_article', ArticleRemoveView.as_view(), name='remove_article'),
+                  url('api/v1/article', ArticleView.as_view(), name='get_article'),
+                  url('api/v1/top_likes_article', ArticleMostLikes.as_view(), name='top_likes_article'),
+                  url('api/v1/top_comments_article', ArticleMostComments.as_view(), name='top_comments_article'),
+                  url('api/v1/top_likes_comment', CommentMostLikes.as_view(), name='top_likes_comment'),
+                  url('api/v1/article_comments', ArticleViewComments.as_view(),
+                      name='get_article_comments'),
+                  url('api/v1/article_reactions', ArticleReactionView.as_view(),
+                      name='get_article_reactions'),
+                  url('api/v1/comment_reactions', CommentReactionView.as_view(),
+                      name='get_comment_reactions'),
                   url(r'^api/v1/add_symbol', SymbolAddView.as_view(), name='add_symbol'),
-                  url('api/v1/remove_symbol/(?P<pk>\d+)/$', SymbolRemoveView.as_view(), name='remove_symbol'),
-                  url('api/v1/update_symbol/(?P<pk>\d+)/$', SymbolUpdateView.as_view(), name='update_symbol'),
+                  url('api/v1/remove_symbol', SymbolRemoveView.as_view(), name='remove_symbol'),
+                  url('api/v1/update_symbol', SymbolUpdateView.as_view(), name='update_symbol'),
                   url(r'^api/v1/symbols', SymbolListView.as_view(), name='symbols'),
+                  url(r'^api/v1/wordcount', WordcountListView.as_view(), name='wordcounts'),
                   url(r'^api/v1/add_comment', CommentAddView.as_view(), name='add_comment'),
+                  url('api/v1/remove_comment', CommentRemoveView.as_view(), name='remove_comment'),
+                  url(r'^api/v1/add_reaction', ArticleReactionAddView.as_view(), name='add_reaction'),
+                  url(r'^api/v1/comment_reaction', CommentReactionAddView.as_view(), name='add_reaction_comment'),
                   # auth
                   path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
                   # swagger docs
@@ -57,3 +74,9 @@ urlpatterns = [
                   # the 'api-root' from django rest-frameworks default router
                   re_path(r'^$', RedirectView.as_view(url=reverse_lazy('api-root'), permanent=False)),
               ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+if settings.DEBUG:
+    import debug_toolbar
+
+    urlpatterns = [re_path(r'^__debug__/', include(debug_toolbar.urls))] + urlpatterns
